cd6a401363df108eb71eac0efae38fb9a0de6954
Jireh <writer.jirehgibson@gmail.com>
2020-05-22 13:19:45 -0400

    adding header components and links to home

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 
 function App() {
   return (
     <div>
-      <switch>
+      <Header />
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-      </switch>
+      </Switch>
     </div>
   );
 }
